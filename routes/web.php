8684404c446f4e0faf1b96da7f6b6918764c2cbc
angeloarcillas <angeloarcillas512@gmail.com>
2020-11-09 10:16:12 +0800

    Refactor web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\{
-    HeroController,
-    HeroPostController,
-    PostController,
-    PostCommentController,
-    CommentController,
-    PostLikeController,
-    CommentLikeController
-};
+use App\Http\Controllers\HeroController;
+use App\Http\Controllers\HeroPostController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostCommentController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostLikeController;
+use App\Http\Controllers\CommentLikeController;
 
-use App\Http\Livewire\{FollowButton, CreatePost};
+use App\Http\Livewire\FollowButton;
+use App\Http\Livewire\CreatePost;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +22,8 @@ use App\Http\Livewire\{FollowButton, CreatePost};
 | contains the "web" middleware group. Now create something great!
 |
 */
-// TODO: Refactor auth middleware
 
-// Auth::login(App\Models\User::first());
+// Auth::login(App\Models\User::Factory->create());
 // Auth::logout();
 
 Route::get('/', function () {
@@ -36,32 +34,24 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
 
     // HERO
-    Route::get('/user/hero/{hero}/edit', [HeroController::class, 'edit'])->name('hero.edit');
-    Route::put('/user/hero/{hero}', [HeroController::class, 'update'])->name('hero.update');
+    Route::get('/users/heroes/{hero}/edit', [HeroController::class, 'edit'])->name('hero.edit');
+    Route::put('/users/heroes/{hero}', [HeroController::class, 'update'])->name('hero.update');
+    Route::post('/heroes/{hero}/follow', FollowButton::class)->name('hero.follow');
 
     // POST
-    Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
-    Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-    Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
-    Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+    Route::post('/heroes/{hero}/posts', CreatePost::class)->name('hero.post.store');
+    Route::resource('posts', PostController::class)->except('index', 'create');
 
     // COMMENT
-    Route::post('/post/{post}/comment', PostCommentController::class, )->name('post.comment.store');
-    Route::get('/comment/{comment}/edit', [CommentController::class, 'edit'])->name('comment.edit');
-    Route::put('/comment/{comment}', [CommentController::class, 'update'])->name('comment.update');
-    Route::delete('/comment/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy');
+    Route::post('/posts/{post}/comments', PostCommentController::class, )->name('post.comment.store');
+    Route::resource('comments', CommentController::class)->only('edit', 'update', 'destroy');
 
     // LIKE
-    Route::post('/post/{post}/like', [PostLikeController::class, 'store'])->name('post.like');
-    Route::post('/comment/{comment}/like', [CommentLikeController::class, 'store'])->name('comment.like');
-    Route::delete('/comment/{comment}/dislike', [CommentLikeController::class, 'destroy'])->name('comment.dislike');
-
-    // FOLLOW
-    Route::post('/hero/{hero}/follow', FollowButton::class)->name('hero.follow');
-    Route::post('/hero/{hero}/post', CreatePost::class)->name('hero.post.store');
-
+    Route::post('/posts/{post}/like', [PostLikeController::class, 'store'])->name('post.like');
+    Route::post('/comments/{comment}/like', [CommentLikeController::class, 'store'])->name('comment.like');
+    Route::delete('/comments/{comment}/dislike', [CommentLikeController::class, 'destroy'])->name('comment.dislike');
 });
 
-Route::get('/user/hero/{hero}', [HeroController::class, 'show'])->name('hero.show');
+Route::get('/users/heroes/{hero}', [HeroController::class, 'show'])->name('hero.show');
 Route::view('/users', 'users', ['users' => App\Models\User::latest()->paginate(20)]);
 Route::view('/posts', 'post/index', ['posts' => App\Models\Post::latest()->paginate(20)]);
