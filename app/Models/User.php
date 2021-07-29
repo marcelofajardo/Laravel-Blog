d59f384e6fc1e4c2aa17ebb5e056335af56f67a8
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-29 10:33:31 +0800

    Add todo

--- app/Models/User.php
+++ app/Models/User.php
@@ -86,6 +86,7 @@ class User extends Authenticatable
     }
 
     /**
+     * TODO: make followable or move to hero model
      * Get the user's following heroes
      */
     public function following()
