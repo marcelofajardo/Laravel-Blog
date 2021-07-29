e22020413d6702fb757f8f5401a7388405d51883
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-29 08:50:35 +0800

    Formatted

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,39 +1,33 @@
 <div class="relative">
-  <img class="object-cover h-48 w-full rounded"
-    src="https://picsum.photos/id/123/200"
-    alt="cover" />
-  <img
-    class="rounded-full h-32 w-32 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
-    style="left: 50%" src="{{$hero->avatar}}"
-    alt="user" />
+    <img class="object-cover h-48 w-full rounded" src="https://picsum.photos/id/123/200" alt="cover" />
+    <img class="rounded-full h-32 w-32 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2" style="left: 50%"
+        src="{{$hero->avatar}}" alt="user" />
 </div>
 
-<div
-  class="flex justify-between items-start my-4">
-  <div class="p-2" style="max-width: 150px">
-  <p class="text-sm text-blue-400">{{'@' . $hero->user->username}}</p>
-    <h2 class="font-bold my-1">
-      {{$hero->user->name}}
-    </h2>
-    <p class="text-xs text-gray-700">
-      Joined
-      {{$hero->created_at->diffForHumans()}}
-    </p>
-  </div>
-  @auth
+<div class="flex justify-between items-start my-4">
+    <div class="p-2" style="max-width: 150px">
+        <p class="text-sm text-blue-400">{{'@' . $hero->user->username}}</p>
+        <h2 class="font-bold my-1">
+            {{$hero->user->name}}
+        </h2>
+        <p class="text-xs text-gray-700">
+            Joined
+            {{$hero->created_at->diffForHumans()}}
+        </p>
+    </div>
+    @auth
     @if ($hero->isOwned())
-      <a href="{{$hero->path('edit')}}"
-        class="px-4 py-2 mt-4 text-xs font-semibold text-blue-400
+    <a href="{{$hero->path('edit')}}" class="px-4 py-2 mt-4 text-xs font-semibold text-blue-400
         border border-blue-400 rounded hover:bg-blue-400 hover:text-white">Edit Profile
-      </a>
+    </a>
     @else
-      @livewire('follow-button', ['hero'=> $hero])
+    @livewire('follow-button', ['hero'=> $hero])
     @endif
-  @endauth
+    @endauth
 </div>
 
 <p class="mb-6 text-sm text-gray-600">
-  {{$hero->bio ?? "Update your bio.."}}
+    {{$hero->bio ?? "Update your bio.."}}
 </p>
 
 
@@ -66,33 +60,31 @@ class="rounded-full mr-2 absolute bottom-0
 transform -translate-x-1/2 translate-y-1/2"
 style="left: 50%" width="100" height="100">
 </div>
-<div
-  class="flex justify-between items-center mb-6">
-  <div>
-    <h2 class="font-bold text-2xl mt-8 mb-2">
-      {{$hero->user->name}}
-    </h2>
-    <p class="text-xs text-gray-700">
-      {{ '@'.$hero->user->username }}</p>
-    <p class="text-xs text-gray-800">Joined
-      {{ $hero->user->created_at->diffForHumans() }}
-    </p>
-  </div>
+<div class="flex justify-between items-center mb-6">
+    <div>
+        <h2 class="font-bold text-2xl mt-8 mb-2">
+            {{$hero->user->name}}
+        </h2>
+        <p class="text-xs text-gray-700">
+            {{ '@'.$hero->user->username }}</p>
+        <p class="text-xs text-gray-800">Joined
+            {{ $hero->user->created_at->diffForHumans() }}
+        </p>
+    </div>
 
-  <div class="flex">
-    <a href="{{$hero->path('edit')}}"
-      class="rounded border border-gray-300 py-2 px-4 text-black text-xs mr-2">
-      Edit Profile
-    </a>
-  </div>
+    <div class="flex">
+        <a href="{{$hero->path('edit')}}" class="rounded border border-gray-300 py-2 px-4 text-black text-xs mr-2">
+            Edit Profile
+        </a>
+    </div>
 </div>
 
 <p class="text-sm text-gray-900">
-  {{$hero->bio ?? 'Update your bio..'}}
-  Lorem ipsum dolor sit amet consectetur
-  adipisicing elit. Corporis molestias iure porro!
-  Consectetur, omnis reiciendis numquam, quaerat
-  totam labore atque accusantium facere natus
-  neque, amet error quibusdam maxime ut vel.
+    {{$hero->bio ?? 'Update your bio..'}}
+    Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Corporis molestias iure porro!
+    Consectetur, omnis reiciendis numquam, quaerat
+    totam labore atque accusantium facere natus
+    neque, amet error quibusdam maxime ut vel.
 </p>
 </div> --}}
