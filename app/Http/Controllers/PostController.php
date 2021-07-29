9881d9ad632cc47b53bb0c1d659854a6a500da84
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-29 17:25:53 +0800

    Refactor post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
@@ -10,33 +11,21 @@ class PostController extends Controller
     public function show(Post $post)
     {
         $post->increment('views_count');
-        return view('post.show', [
-            'post' => $post,
-            'comments' => $post->comments
-        ]);
+        return view('post.show', compact('post'));
     }
 
-    // TODO: convert to livewirr
     public function edit(Post $post)
     {
         $this->authorize('update', $post);
         return view('post.edit', compact('post'));
     }
 
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        $this->authorize('update', $post);
-
-        $validatedData = $request->validate([
-            'body' => 'required|min:5',
-        ]);
-
-        $post->update($validatedData);
-
-        return redirect("/post/{$post->id}")->with('success', 'post successfuly updated.');
+        $post->update($request->validated());
+        return redirect("/posts/{$post->id}")->with('success', 'post successfuly updated.');
     }
 
-    // TODO: convert to livewire
     public function destroy(Post $post)
     {
         $this->authorize('delete', $post);
