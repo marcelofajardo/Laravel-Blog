ecb7352b4ff0692d7542cfd1720af6c986ded417
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-30 16:02:46 +0800

    refactor post.index view

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,12 +1,15 @@
-<x-master>
-  <x-slot name="header">Posts</x-slot>
+<x-app-layout>
+    <x-slot name="header">Posts</x-slot>
 
-  <div class="mb-6">
-     @foreach ($posts as $post)
-    <div
-      class="{{$loop->last ? '' : 'border-b'}} border-gray-200 p-2 pb-6">
-      <x-post :post="$post"></x-post>
+    <div class="mb-6">
+        @foreach ($posts as $post)
+        <div class="{{$loop->last ? '' : 'border-b'}} border-gray-200 p-2 pb-6">
+            @livewire('post.show', ['post' => $post])
+        </div>
+        @endforeach
+
+        <div>
+            {{ $posts->links() }}
+        </div>
     </div>
-    @endforeach
-  </div>
-</x-master>
+</x-app-layout>
