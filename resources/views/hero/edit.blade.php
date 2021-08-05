179908310c16e75ac0098535cc719c60fe7ca948
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-08-05 10:28:07 +0800

    Refactor: remove unused files & improve code

--- resources/views/hero/edit.blade.php
+++ resources/views/hero/edit.blade.php
@@ -5,33 +5,32 @@
     </h2>
   </x-slot>
 
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-        <form action="{{$hero->path('update')}}" method="POST">
-          @method('PUT')
-          @csrf
-          <div class="mb-6">
-            <label class="mb-2 block text-sm font-bold text-gray-700 uppercase">Body</label>
-            <textarea class="w-full border px-4 py-2 rounded outline-none
-                                    text-gray-700 @error('bio') border-red-500 @enderror
-                          focus:border-blue-500 focus:shadow-outline" name="bio" rows="10"
-              placeholder="Write something..">{{$hero->bio}}</textarea>
-            @error('bio')
-            <span class="text-xs text-red-400 mt-1 italic">
-              {{$message}}
-            </span>
-            @enderror
-          </div>
+  <div>
+    <form action="{{$hero->path('update')}}" method="POST">
+      @method('PUT')
+      @csrf
 
-          <div class="mb-6 flex items-center">
-            <button class="bg-blue-400 text-white rounded py-2 px-8
-                                hover:bg-blue-500" type="submit">Update
-            </button>
-            <a class="ml-4 text-red-400 hover:text-red-500" href="/users/heroes/{{$hero->id}}">Cancel</a>
-          </div>
-        </form>
+      {{-- BIO --}}
+      <div class="mb-6">
+        <label class="mb-2 block text-sm font-bold text-gray-700 uppercase">Body</label>
+        <textarea name="bio" rows="10"
+          class="w-full border px-4 py-2 rounded outline-none text-gray-700
+          focus:border-blue-500 focus:shadow-outline"
+          @error('bio') border-red-500 @enderror
+          placeholder="Write something.."
+          >{{$hero->bio}}</textarea>
+
+          <x-form.inline-error field="bio"></x-form.inline-error>
       </div>
-    </div>
+
+      {{-- EDIT CONTROLS --}}
+      <div class="mb-6 flex items-center">
+        <x-form.button label="Update"></x-form.button>
+        <a href="/users/heroes/{{$hero->id}}"
+          class="ml-4 text-red-400 hover:text-red-500"
+          >Cancel
+        </a>
+      </div>
+    </form>
   </div>
 </x-app-layout>
--- resources/views/hero/show.blade.php
+++ resources/views/hero/show.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-    <x-slot name="header">Hero</x-slot>
+  <x-slot name="header">Hero</x-slot>
 
-    <div class="mb-6 rounded shadow p-2">
-        <x-hero :hero="$hero"></x-hero>
-    </div>
+  <div class="mb-6 rounded shadow p-2">
+    <x-hero :hero="$hero"></x-hero>
+  </div>
 
 
-    <div class="rounded shadow">
-        @livewire('post.index', [
-            'hero' => $hero,
-            'posts' => $posts
-            ])
-    </div>
+  <div class="rounded shadow">
+    @livewire('post.index', [
+      'hero' => $hero,
+      'posts' => $posts
+    ])
+  </div>
 </x-app-layout>
