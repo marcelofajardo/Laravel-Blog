2d13d1fb72035ccd888046c0c0f5ca85073cadef
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-08-05 11:25:36 +0800

    Refactor: Improve code

--- resources/views/livewire/comment/index.blade.php
+++ resources/views/livewire/comment/index.blade.php
@@ -1,9 +1,12 @@
 <div>
-  @foreach ($post->comments as $comment)
-  <div class="{{$loop->last ? 'mb-6' : 'border-b'}} border-gray-200 p-2 pb-6">
-    @livewire('comment.show', ['comment' => $comment], key($comment->id))
-  </div>
-  @endforeach
+  @forelse ($post->comments as $comment)
+    <div class="{{$loop->last ? 'mb-6' : 'border-b'}} border-gray-200 p-2 pb-6">
+      @livewire('comment.show', ['comment' => $comment], key($comment->id))
+    </div>
+
+  @empty
+    <p>No comment available.</p>
+  @endforelse
 
   <div>
     @livewire('comment.create', ['post' => $post])
--- resources/views/livewire/comment/show.blade.php
+++ resources/views/livewire/comment/show.blade.php
@@ -1,5 +1,4 @@
 <div class="flex">
-
   {{-- LIKE/DISLIKE --}}
   <div class="flex justify-between items-center flex-col px-4 text-sm text-gray-700">
     <button wire:click="like" class="hover:text-blue-700 {{! $comment->isLiked() ?: 'text-blue-700'}}">
@@ -15,48 +14,50 @@
     </button>
 
     {{-- long post indicator --}}
-    <span class="mt-4 bg-gray-300 h-full w-1">
-    </span>
+    <span class="mt-4 bg-gray-300 h-full w-1"></span>
   </div>
 
   {{-- COMMENT BODY --}}
   <div class="w-full">
     @if (!$isEdit_able)
-    <p class="mb-2 text-gray-700 text-sm">
-      {{ $comment->body }}
-    </p>
-    <div class="flex justify-between items-center">
-      <p class="text-xs text-gray-700">
-        <a class="font-bold hover:text-blue-400" href="#">{{ '@'.$comment->user->name }}
-        </a>
-        &bull; 3 mins ago
-      </p>
+      <p class="mb-2 text-gray-700 text-sm">{{ $comment->body }}</p>
+      <div class="flex justify-between items-center">
+        <p class="text-xs text-gray-700">
+          <a href="/users/heroes/{{ $comment->user->id }}"
+            class="font-bold hover:text-blue-400"
+            >{{ '@' . $comment->user->name }}
+          </a>
+          &bull; 3 mins ago
+        </p>
 
-      {{-- COMMENT CONTROLS --}}
-      <div class="text-xs">
-        @can(['update','delete'], $comment)
-        <button wire:click="showEdit" class="text-blue-400 hover:text-blue-500 hover:underline mr-1">
-          Edit
-        </button>
+        {{-- COMMENT CONTROLS --}}
+        <div class="text-xs">
+          @can(['update','delete'], $comment)
+          <button wire:click="showEdit" class="text-blue-400 hover:text-blue-500 hover:underline mr-1">
+            Edit
+          </button>
 
-        <button wire:click="delete" class="text-red-400 over:text-red-500 hover:underline">
-          Delete
-        </button>
-        @endcan
+          <button wire:click="delete" class="text-red-400 over:text-red-500 hover:underline">
+            Delete
+          </button>
+          @endcan
+        </div>
       </div>
-    </div>
+
     @else
-    <form wire:submit.prevent="update" action="#">
-      <div class="mb-6">
-        <label for="body" class="block mb-1 text-sm font-bold text-gray-500">Comment</label>
-        <textarea wire:model="body" name="body" id="body" class="w-full p-2 border rounded"
-          rows=10>{{$this->comment->body}}</textarea>
-        @error('body') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-      </div>
-      <div class="text-right">
-        <x-form.button label="Update"></x-form.button>
-      </div>
-    </form>
+      <form wire:submit.prevent="update" action="#">
+        {{-- BODY --}}
+        <div class="mb-6">
+          <label for="body" class="block mb-1 text-sm font-bold text-gray-500">Comment</label>
+          <textarea wire:model="body" name="body" id="body" class="w-full p-2 border rounded"
+            rows=10>{{$this->comment->body}}</textarea>
+          @error('body') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
+        </div>
+        {{-- CONTROLS --}}
+        <div class="text-right">
+          <x-form.button label="Update"></x-form.button>
+        </div>
+      </form>
     @endif
   </div>
 </div>
